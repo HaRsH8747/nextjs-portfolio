78cdc36c157045466ada52d44eecb2fb0af6eddf
HaRsH8747 <harshmoradiya8747@gmail.com>
2024-05-25 19:02:03 -0400

    About Update

--- components/contact/ContactDetails.jsx
+++ components/contact/ContactDetails.jsx
@@ -3,19 +3,14 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
-		icon: <FiMapPin />,
-	},
-	{
-		id: 2,
-		name: 'email@domain.com',
+		name: 'harshmoradiya8747@gmail.com',
 		icon: <FiMail />,
 	},
-	{
-		id: 3,
-		name: '555 8888 888',
-		icon: <FiPhone />,
-	},
+	// {
+	// 	id: 2,
+	// 	name: '+1 (226) 9615-961',
+	// 	icon: <FiPhone />,
+	// },
 ];
 
 function ContactDetails() {
